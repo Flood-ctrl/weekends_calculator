0458a0faaa5c243fd1590b3d43737fad782181b0
Serhii Novokhatskyi <serhiinovokhatskyi@Serhiis-MacBook-Air.local>
2021-11-25 19:45:45 +0200

    Add calendar

--- weekends_calc.py
+++ weekends_calc.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
 import datetime
+import calendar
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 current_year = datetime.datetime.now().year
 start_work_day = datetime.date(current_year, 11, 22)
 weekend = datetime.timedelta(days=4) + start_work_day
 day_of_week = weekend.isoweekday()
+calendar_month = calendar.TextCalendar(calendar.SUNDAY)
 
 if day_of_week != 6:
     while day_of_week != 6:
@@ -13,10 +26,10 @@ if day_of_week != 6:
         day_of_week = next_day_postition.isoweekday()
         if day_of_week != 6:
             weekend = next_day_postition
-    print(next_day_postition)
+    print(f"{bcolors.WARNING}{next_day_postition}{bcolors.ENDC}")
+    c_str = calendar_month.formatmonth(next_day_postition.year,next_day_postition.month)
+    print(f"{bcolors.OKGREEN}{c_str}{bcolors.ENDC}")
 elif day_of_week == 6:
-    print(weekend)
-
-# print(start_work_day)
-#print(weekend)
-#print(day_of_week)
+    print(f"{bcolors.WARNING}{weekend}{bcolors.ENDC}")
+    c_str = calendar_month.formatmonth(weekend.year,weekend.month)
+    print(f"{bcolors.OKCYAN}{c_str}{bcolors.ENDC}")
