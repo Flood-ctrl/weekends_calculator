38437f8a6c6ec85111f1241d313d0117ce7acdd3
Serhii Novokhatskyi <serhiinovokhatskyi@Serhiis-MacBook-Air.local>
2021-11-25 18:46:17 +0200

    Initial commit

--- weekends_calc.py
+++ weekends_calc.py
@@ -2,17 +2,21 @@
 
 import datetime
 
-currentYear = datetime.datetime.now().year
-
-start_work_day = datetime.date(currentYear, 11, 22)
-
+current_year = datetime.datetime.now().year
+start_work_day = datetime.date(current_year, 11, 22)
 weekend = datetime.timedelta(days=4) + start_work_day
-
 day_of_week = weekend.isoweekday()
 
+if day_of_week != 6:
+    while day_of_week != 6:
+        next_day_postition = weekend + datetime.timedelta(days=6)
+        day_of_week = next_day_postition.isoweekday()
+        if day_of_week != 6:
+            weekend = next_day_postition
+    print(next_day_postition)
+elif day_of_week == 6:
+    print(weekend)
 
-
-
-print(start_work_day)
-print(weekend)
-print(day_of_week)
+# print(start_work_day)
+#print(weekend)
+#print(day_of_week)
